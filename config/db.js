a23e8a451bcc2f169b053bbf2e218118c1b77943
Armaan2022 <armaan.das2017@gmail.com>
2026-04-12 12:30:26 -0600

    Prepared backend for deployment

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = async() => {
-    //return mongoose.connect(process.env.MONGODB_URL)
-    return mongoose.connect("mongodb://127.0.0.1/contacts_data")
+    return mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log("Connection to database established..."))
   .catch((err) => console.error("Error connecting to database:", err));
 }; 
