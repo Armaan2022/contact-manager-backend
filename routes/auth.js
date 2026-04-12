c98653554a437150995d7594d791b4bad859b7e3
Armaan2022 <armaan.das2017@gmail.com>
2026-04-12 12:48:05 -0600

    Replaced bcrypt with bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { FindCursor } = require('mongodb');
 const User = require("../models/User")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 const auth = require("../middlewares/auth");
